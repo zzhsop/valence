e7fd2473988efbd944c58655f27a71b2f2608db0
admin <admin@test.com>
2024-01-17 09:29:37 +0800

    YW-20240117: auto add commit

---
+++ pushgitrepo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WORK_DIR="$(cd "$(dirname $0)" && pwd)"
+cd $WORK_DIR || exit 1
+
+commit="YW-$(date +%Y%m%d): auto add commit"
+# shellcheck disable=SC1072
+if [ -n "$1" ];then
+ commit=$1
+fi
+
+git add --all && git commit -m "$commit" && git push --set-upstream origin master
+
+git push origin master:release
+
+echo "https://raw.githubusercontent.com/zzhsop/zzhsop/release/xxx.json"
